a711ce08c3627514850821427cb97aa0f91867b6
NotAShelf <raf@notashelf.dev>
2025-10-09 17:22:33 +0300

    meta: disable trailing slash for astro URLs
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I1dd81621798bb49d4ddbab165f7456456a6a6964

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,13 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://lists.notashelf.dev",
   output: "static",
+  trailingSlash: "never",
   build: {
     format: "directory",
+  },
+  vite: {
+    build: {
+      assetsInlineLimit: 0,
+    },
   },
 });
