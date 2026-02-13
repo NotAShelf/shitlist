b8cae8bd51821f903c0e65ada2b23995389238f8
NotAShelf <raf@notashelf.dev>
2026-02-13 12:02:26 +0300

    content: force excerpt
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I179e71a85ee158c7120faf64a7fb58656a6a6964

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,26 +1,24 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z } from 'astro:content';
 
-const shitCollection = defineCollection({
-  type: "content",
-  schema: z.object({
+const entrySchema = z.object({
     title: z.string(),
+    excerpt: z.string().optional(),
     tags: z.array(z.string()).optional(),
     dateAdded: z.date(),
-    status: z.enum(["active", "outdated", "archived"]).default("active"),
-  }),
+    status: z.enum(['active', 'outdated', 'archived']).default('active'),
+});
+
+const shitCollection = defineCollection({
+    type: 'content',
+    schema: entrySchema,
 });
 
 const starsCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    tags: z.array(z.string()).optional(),
-    dateAdded: z.date(),
-    status: z.enum(["active", "outdated", "archived"]).default("active"),
-  }),
+    type: 'content',
+    schema: entrySchema,
 });
 
 export const collections = {
-  shit: shitCollection,
-  stars: starsCollection,
+    shit: shitCollection,
+    stars: starsCollection,
 };
